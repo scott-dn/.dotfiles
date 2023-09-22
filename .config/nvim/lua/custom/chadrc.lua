aa75b018352517268a6861b865216972fd9b3412
Scott Dn <scott.dn91@gmail.com>
2023-09-23 02:09:01 +0700

    chadrc ui

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,6 +1,14 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = 'onedark' }
+M.ui = {
+  theme = 'onedark',
+  statusline = {
+    theme = 'vscode_colored'
+  },
+  tabufline = {
+    show_numbers = true
+  }
+}
 M.plugins = 'custom.plugins'
 M.mappings = require 'custom.mappings'
 return M
