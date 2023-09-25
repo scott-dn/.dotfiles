fb3e7ea8602786e0ab1c6dc560c1dc70842db9ac
Scott Dn <scott.dn91@gmail.com>
2023-09-26 02:17:56 +0700

    auto save

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -11,3 +11,12 @@ vim.opt.list = true
 vim.opt.listchars = { eol = "¬", tab = "\\|\\", trail = "▫", extends = ">", precedes = "<", space = "·" }
 
 vim.g.mapleader = ","
+
+-- auto save
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+      vim.api.nvim_command("silent update")
+    end
+  end,
+})
